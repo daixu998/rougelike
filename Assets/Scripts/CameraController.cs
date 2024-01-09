aeec185fef133e61926e52ab773f8c538879e78e
aksjd <181258703@qq.com>
2024-01-09 23:16:41 +0800

    3

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position =new Vector3(target.position.x,target.position.y,transform.position.z) ;
+    }
+}
